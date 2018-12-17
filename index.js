efd137b5efc4a6beb0dcdf4d06c996e85f5b612a
호근이 <hogueny@gmail.com>
2018-12-17 23:22:21 +0900

    12/17 routing

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var http = require('http');
-var server = http.createServer();
-
-var host = "192.168.0.1";
-var port = 3000;
-server.listen(port,3000,function () {
-    console.log('server start');
-
-});
-
-server.on('request', function (req, res) {
-    console.log('access');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("hello");
-    res.end();
-});
-
-server.on('close',function () {
-    console.log('end')
 
 
-});
+var option = {
+    host : 'localhost',
+    port : '8081',
+    path : '/index.html'
+};
 
+var callback = function (response) {
+    var body='';
+    response.on('data',function (data) {
+        body += data;
+    });
+
+    response.on('end',function() {
+        console.log(body);
+    });
+}
+
+var req = http.request(option,callback);
+req.end();
--- server.js
+++ server.js
@@ -1,31 +1,11 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
+var router = require('./routes/router')(app);
 
+app.set('views', __dirname + '/views');
+app.set('views engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-// 서버 생성
-http.createServer( function (request, response) {
-    var pathname = url.parse(request.url).pathname;
-
-
-    console.log("Request for " + pathname + " received.");
-
-
-    if(pathname=="/"){
-        pathname = "/index.html";
-    }
-
-    fs.readFile(pathname.substr(1), function (err, data) {
-        if (err) {
-            console.log(err);
-            response.writeHead(404, {'Content-Type': 'text/html'});
-        }else{
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data.toString());
-        }
-        response.end();   // responsebody 전
-    });
-}).listen(8081);
-
-
-console.log('Server running at http://127.0.0.1:8081/');
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+});
