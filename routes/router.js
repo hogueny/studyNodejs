5b432ee042fe8a908fd06510edd45a96ce02e35e
호근이 <hogueny@gmail.com>
2018-12-19 15:16:10 +0900

    router

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,15 @@
-module.exports = function (app) {
-    app.get('/', function (req,res) {
-        res.render('./index.html')
-    });
-    app.get('/about', function (req,res) {
-        res.render('./about.html')
-    });
+exports.index = function(req,res){
+    res.render('index.html');
+}
+
+exports.login = function(req,res){
+    res.render('login.html');
+}
+
+exports.about = function(req,res){
+    res.render('about.html');
+}
+
+exports.join = function(req,res){
+    res.render('join.html');
 }
