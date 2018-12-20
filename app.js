78da56557d9d80bf86b44b1ba1f2c1fd6518ec56
호근이 <hogueny@gmail.com>
2018-12-20 17:48:06 +0900

    param

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
+// express 기본 모듈
 var express = require('express')
     , routes = require('./routes/router')
     , https = require('http')
-    , path = require('path')
-    , ejs = require('ejs');
-
-
+    , path = require('path');
+// express 미들웨어
+var ejs = require('ejs')
+    , static = require('serve-static')
+    , bodyParser = require('body-parser');
 var app = express();
 
+//기본속성
 app.set('port', process.env.PORT || 3000);
-app.set('views',__dirname + '/views');
+//app.set('views',__dirname + '/views');
 app.engine('.html',ejs.__express);
 app.set('view engine','html');
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-///
+// body-parser를 이용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/view',static(path.join(__dirname,'view'))); 
+
+
+///라우팅
 app.get('/',routes.index);
 app.get('/login',routes.login);
 app.get('/about',routes.about);
 app.get('/join',routes.join);
+app.get('/join_ok',routes.join_ok);
+
+// 미들웨어
+app.use(function(req,res,next){
+    console.log('파라미터 주고받기 미들웨어');
+
+    var parID = req.body.id || req.query.id;
+    var parPW = req.body.pw || req.query.pw;
+    
+    res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
+    res.write('<h1>Express 서버에서 응답한 결과</h1>');
+    res.write('<div><p>ParID : '+parID+'</p></div>');
+    res.write('<div><p>ParPW : '+parPW+'</p></div>');
+    res.end();
+
+})
+
+
+
 //server start
 https.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));;
--- routes/router.js
+++ routes/router.js
@@ -6,10 +6,18 @@ exports.login = function(req,res){
     res.render('login.html');
 }
 
+exports.login_ok = function(req,res){
+    res.render('login_ok.html');
+}
+
 exports.about = function(req,res){
     res.render('about.html');
 }
 
 exports.join = function(req,res){
     res.render('join.html');
+}
+
+exports.join_ok = function(req,res){
+    res.render('join_ok.html');
 }
