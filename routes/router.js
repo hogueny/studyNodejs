f4ca1bd88139dad68bd699f6a888856ab0817733
호근이 <hogueny@gmail.com>
2018-12-20 17:48:51 +0900

    Revert "param"
    This reverts commit 78da56557d9d80bf86b44b1ba1f2c1fd6518ec56.

--- routes/router.js
+++ routes/router.js
@@ -6,18 +6,10 @@ exports.login = function(req,res){
     res.render('login.html');
 }
 
-exports.login_ok = function(req,res){
-    res.render('login_ok.html');
-}
-
 exports.about = function(req,res){
     res.render('about.html');
 }
 
 exports.join = function(req,res){
     res.render('join.html');
-}
-
-exports.join_ok = function(req,res){
-    res.render('join_ok.html');
 }
